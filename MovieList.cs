10e7e8c83880f83423a5353cf7e8dafe2d5e00a3
t-sylviale <t-sylviale@microsoft.com>
2022-06-30 11:50:57 -0700

    add a movie list

---
+++ MovieList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vsapp
+{
+    internal class MovieList
+    {
+        List<movie> movies;
+
+        public MovieList()
+        {
+            movies = new();
+        }
+
+        public void AddMovie(movie movie)
+        {
+
+        }
+
+        public void Delete(movie movie
+        {
+
+        }
+
+        public void Search(movie movie)
+        {
+
+        }
+
+        public void ModifyMovie(movie movie)
+        {
+
+        }
+
+    }
+}

85d81d32ed78a868ad59bfc3ee00d8bf990cb600
t-sylviale <t-sylviale@microsoft.com>
2022-06-30 11:44:08 -0700

    add a menu

---
+++ Menu.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Menu
+{
+    public static void Main(string[] Args)
+    {
+		Console.Write("What do you want to do");
+        string choice = "A";
+
+        if (choice == "A")
+        {
+            //do sth
+        }
+
+        if (choice == "A")
+        {
+            //do sth
+        }
+        if (choice == "A")
+        {
+            //do sth
+        }
+        if (choice == "A")
+        {
+            //do sth
+        }
+
+    }
+}

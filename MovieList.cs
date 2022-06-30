0ede99fd6f3bb9b50b741adfd90e76b607f27b43
t-sylviale <t-sylviale@microsoft.com>
2022-06-30 11:59:30 -0700

    edit movielist class

--- MovieList.cs
+++ MovieList.cs
@@ -17,22 +17,42 @@ namespace vsapp
 
         public void AddMovie(movie movie)
         {
-
+            movies.Add(movie);
         }
 
-        public void Delete(movie movie
+        public void Delete(movie movie)
         {
-
+            foreach (movie m in movies)
+            {
+                if (movie.Equals(m))
+                {
+                    movies.Remove(m);
+                    break;
+                }
+            }
         }
 
         public void Search(movie movie)
         {
-
+            foreach (movie m in movies)
+            {
+                if (m.Equals(movie))
+                {
+                    return m;
+                }
+            }
         }
 
-        public void ModifyMovie(movie movie)
+        public void ModifyMovie(movie movie, ? newVal)
         {
-
+            foreach (movie m in movies)
+            {
+                if (m.Equals(movie))
+                {
+                    ModifyFunc();
+                    break;
+                }
+            }
         }
 
     }
